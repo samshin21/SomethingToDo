b8c3f9daccf31e422e363af03964c67c2a0ca980
sam <onesam23@gmail.com>
2025-12-11 12:08:55 -0500

    Add: user choice selection going based off CRUD

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,58 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
+// having this namespace makes it so we don't have to put 'std::' on everything
 using namespace std;
 
-int main() {
-	// main function to get user choice
-	usrChoice = 0
+void printUserMenu () {
+	// outputting the user menu
+	cout << "\n*** What do YOU want to do? ***\n";
+	cout << "0. Give me something to do!\n";
+	cout << "1. Create something\n";
+	cout << "2. Read something\n";
+	cout << "3. Update something\n";
+	cout << "4. Destroy something\n";
+	cout << "5. Exit\n";
+}
+
+// using 'addressof' because the coding jesus mentioned it
+void onCreate (const vector<string> addressof(todo)) {
 	
-	// infinite loop to get user choice
-	for(;;) { 
-		// outputting the user menu
-		cout << "\n*** What do YOU want to do? ***\n";
-		cout << "1. Create something\n";
-		cout << "2. Read something\n";
-		cout << "3. Update something\n";
-		cout << "4. Destroy something\n";
-		cout << "5. Exit\n";
-		// inputting the user choice
-		cin >> usrChoice
+}
+
+void onRead (const vector<string> addressof(todo)) {
+}
+void onUpdate (const vector<string> addressof(todo)) {
+}
+void onDestroy (const vector<string> addressof(todo)) {
+}
+
+int main () {
+	// main function to get user choice
+	vector<string> todo
+	usrChoice = 0;
+	printUserMenu();
+
+	// inputting the user choice and clearing 'cin' buffer
+	cin >> usrChoice;
+	cin.ignore ();
+	
+	// infinite loop
+	for (;;) {
+		// per ChatGPT switch statements are faster when dealing with ints
+		// compared to regular if-else statemetns
+		switch (usrChoice) {
+			case 1:
+
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			default:
+				cout << "Invalid Input
+		}
+
 	}
 	return 0;
 }
