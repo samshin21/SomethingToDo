bfec6935c7983f9e05ef460b1cb3b1b7c22767b5
sam <onesam23@gmail.com>
2025-12-11 13:27:02 -0500

    Bug; running into infinite loop issue when user wants to create another item

--- src/main.cpp
+++ src/main.cpp
@@ -16,41 +16,64 @@ void printUserMenu () {
 	cout << "5. Exit\n";
 }
 
-// using 'addressof' because the coding jesus mentioned it
-void onCreate (const vector<string> addressof(todo)) {
-	
-}
-
-void onRead (const vector<string> addressof(todo)) {
-}
-void onUpdate (const vector<string> addressof(todo)) {
-}
-void onDestroy (const vector<string> addressof(todo)) {
+void printUserList (const vector<string>& todo) {
+	if (todo.empty()) {cout << "You have nothing to do";}
+	else {
+		cout << "Here are some things you could do...\n";
+		for (int i = 0; i < todo.size(); i++) {cout << i << ". " << todo[i] << "\n"; }
+	}
 }
 
 int main () {
 	// main function to get user choice
-	vector<string> todo
-	usrChoice = 0;
-	printUserMenu();
-
-	// inputting the user choice and clearing 'cin' buffer
-	cin >> usrChoice;
-	cin.ignore ();
+	vector<string> todo;
+	int usrChoice = 0;
 	
 	// infinite loop
 	for (;;) {
-		// per ChatGPT switch statements are faster when dealing with ints
-		// compared to regular if-else statemetns
-		switch (usrChoice) {
-			case 1:
+		// prompting and recieving user input
+		printUserMenu();
+		cin >> usrChoice;
+		cin.ignore();
 
+		// per ChatGPT switch statements are faster when dealing with ints compared tp regular if-else statements
+		switch (usrChoice) {
+			// Create
+			case 1: {
+				for(;;) {
+					cout << "Enter something to do: ";
+					string itemCreate;
+					getline(cin, itemCreate);
+					todo.push_back(itemCreate);
+					cout << "\nDo you want to create something else to do (Yes/no)?\n";
+					// going to use this variable name for generic user inputs
+					string usr;
+					cin >> usr;
+					// leaving this loop if usr response does NOT contain 'y' or 'Y'
+					if (usr.find("y") == string::npos ||
+						usr.find("Y") == string::npos) {break;}
+				}
+			} break;
+			// Read
 			case 2:
+				printUserList(todo);
+				break;
+			// Update
 			case 3:
+				break;
+			// Destroy
 			case 4:
+				break;
+			// Exit
 			case 5:
+				cout << "Goodbye!\n";
+				// using return instead of exit() since I'm in the main function
+				return 0;
+				// good defensive measure even if you don't need the 'break' statement
+				break;
 			default:
-				cout << "Invalid Input
+				cout << "Invalid Input\n";
+				break;
 		}
 
 	}
