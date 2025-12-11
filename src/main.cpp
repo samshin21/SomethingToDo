f5486d83d4de75758e98d8636120a553acbb4f7a
sam <onesam23@gmail.com>
2025-12-11 11:42:14 -0500

    Initial commit: created user menu

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// main function to get user choice
+	usrChoice = 0
+	
+	// infinite loop to get user choice
+	for(;;) { 
+		// outputting the user menu
+		cout << "\n*** What do YOU want to do? ***\n";
+		cout << "1. Create something\n";
+		cout << "2. Read something\n";
+		cout << "3. Update something\n";
+		cout << "4. Destroy something\n";
+		cout << "5. Exit\n";
+		// inputting the user choice
+		cin >> usrChoice
+	}
+	return 0;
+}
+
