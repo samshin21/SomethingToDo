fe22ae29bd64d3817c171d98adfba2c7a3c1cfbc
sam <onesam23@gmail.com>
2025-12-13 14:04:46 -0500

    UPDATE: added basic implementation of the program, will be more mindful of git pushes

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,18 @@
+// SomethingToDo
+// Date: 12.06.2025
+// Author: Sam Shin
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include <limits>
+#include <random>
 
-// having this namespace makes it so we don't have to put 'std::' on everything
-using namespace std;
+using namespace std;												// declaring namespace so we don't need 'std::'
 
-// DO NOT USE YET
-struct ItemData {
-	string item;
-	double weight;
-};
-
-// outputting user menu
-void printUserMenu () {
-	cout << "\n--- What do YOU want to do? ---\n";
+void printMenu () {
+// function prints the menu for the user to select from
+	cout << "\n--- What do you  want to do? ---\n";
 	cout << "0. Give me something to do!\n";
 	cout << "1. Create something\n";
 	cout << "2. Read something\n";
@@ -23,94 +21,119 @@ void printUserMenu () {
 	cout << "5. Exit\n";
 }
 
-// printing user somethingToDo list
-void printUserList (const vector<string>& todo) {
+int itemRead (const vector<string>& todo) {
+// method reads the list (if there is any) and returns true(1) or false(0)
 	if (todo.empty()) {
-		cout << "You have nothing to do";
+		cout << "You have nothing to do\n";
+		return 0;
 	}
 	else {
 		for (int i = 0; i < todo.size(); i++) {
-			cout << i << ". " << todo[i] << "\n";
+			cout << i << ". " << todo[i] << "\n";								// cout the index and respective item from to do list
 	      	}
 	}
+	return 1;
 }
 
 int main () {
-	// main function to get user choice
+// main method
 	vector<string> todo;
-	int usrChoice;
-	bool validInput = false;
+	int usrIndex;
 	
-	// infinite loop
-	for (;;) {
-		// prompting and recieving user input
-		printUserMenu();
+	for(;;) {													// infinite loop
+		printMenu();
 		
-		// error handling for incorrect input type
-		while (!validInput) {
-			if (cin.fail()) {
-				cout << "\n*** INVALID INPUT: EXPECTING INTEGER ***\n";
-				cin.clear();
-				cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			}
-			else {
-				validInput = true;
-				cin >> usrChoice;
-			}
+		while (!(cin >> usrIndex)) {										// checks if user entered an int
+			cout << "\n*** INVALID INPUT: EXPECTING INTEGER ***\n";
+			cin.clear();											// this clears the error
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');						// this ignores the buffer
 		}
-		validInput = false;
 
-		// per ChatGPT switch statements are faster when dealing with ints compared tp regular if-else statements
-		switch (usrChoice) {
-			// Create
-			case 1: {
+		switch (usrIndex) {
+			case 0: {											// PICKING a random item
+					if (todo.empty()) {
+						cout << "You have nothing to do\n";
+					}
+					else {
+						static random_device itemRandomIndex;					// random_device: uniform -distributed random number generator
+						static mt19937 gen(itemRandomIndex());					// mt19937: pseudo-random generator of 32-bit numbers with a state size of 19937 bits
+						uniform_int_distribution<size_t> dist(0, todo.size()-1);		// setting the random number limits
+						
+						string itemRandom = todo[dist(gen)];
+						cout << "Do this: " + itemRandom + "\n";
+					}
+				} break;
+			
+			case 1: {											// CREATE an item for to do list
 					cout << "Enter something to do: ";
 					string itemCreate;
 					getline(cin >> ws, itemCreate);
-					todo.push_back(itemCreate);
-					cout << "Added something to do\n";
-				} break;
-			// Read
-			case 2:
-				cout << "Here are some things you could do...\n";
-				printUserList(todo);
-				break;
-			// Update
-			case 3: {
-					cout << "Here is your current to do list...\n";
-					printUserList(todo);
-					cout << "which index do you want to update?\n";
-					int index;
-					cin >> index;
-					// error handling for user input
-					while (!validInput) {
-						if (cin.fail()) {
-							cout << "\n*** INVALID INPUT: EXPECTING INTEGER ***\n";
-							cin.clear();
-							cin.ignore(numeric_limits<streamsize>::max(), '\n');
-						}
-						else {
-							validInput = true;
-							cin >> usrChoice;
-						}
-					}
-					validInput = false;
 					
-					// checking to see if index is within the todo list
-					if (index >= 0 && index < todo.size()) {
-						cout << "Enter the updated task: ";
-						string itemUpdate;
-						getline(cin, itemUpdate);
-						todo[index] = itemUpdate;
+					todo.push_back(itemCreate);							// adds an item to the end of the string vector
+					cout << "Added: " + itemCreate + "\n";
+				} break;
+
+			case 2:												// READ the items in the current to do list
+				cout << "Here are some things you could do...\n";
+				itemRead(todo);
+				break;
+
+			case 3: {											// UPDATE an item in the current to do list
+	  				cout << "Here are some things you can update...\n";
+					if (itemRead(todo) == 0) {
+						break;
+					}
+					
+					cout << "Which index do you want to update?\n";
+					int itemUpdateIndex;
+					
+					while (!(cin >> itemUpdateIndex)) {						// ensuring user input is a string
+						cout << "\n*** INVALID INPUT: EXPECTING INTEGER ***\n";
+						cin.clear();
+						cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					}
+
+					if (0 > itemUpdateIndex || itemUpdateIndex >= todo.size()) {			// ensuring user input falls within vector's size
+						cout << "\n***INVALID INPUT: INDEX OUT OF RANGE ***\n";
+						cin.ignore(numeric_limits<streamsize>::max(), '\n');
 					}
 					else {
-						cout << "*** INVALID INPUT: INTEGER OUT OF RANGE ***\n";
+						cout << "Enter your updated item: ";
+						string itemNew, itemOld;
+						getline(cin >> ws, itemNew);
+						
+						itemOld = todo[itemUpdateIndex];
+						todo[itemUpdateIndex] = itemNew;
+						cout << "Updated: " + itemOld + " to " + itemNew + "\n";
 					}
 				} break;
-			// Destroy
-			case 4:
-				break;
-			// Exit
+
+			case 4: {											// DESTROY an item from the current list
+	  				cout << "Here are some things you can destroy...\n";
+					if (itemRead(todo) == 0) {
+						break;
+					}
+					
+					cout << "Which index do you want to destroy\n";
+					int itemDeleteIndex;
+					
+					while (!(cin >> itemDeleteIndex)) {						// ensuring user input is a string
+						cout << "\n*** INVALID INPUT: EXPECTING INTEGER ***\n";
+						cin.clear();
+						cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					}
+
+					if (0 > itemDeleteIndex || itemDeleteIndex >= todo.size()) {			// ensuring user input falls within vector's size
+						cout << "\n***INVALID INPUT: INDEX OUT OF RANGE ***\n";
+						cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					}
+					else {
+						string itemDelete = todo[itemDeleteIndex];
+						todo.erase(todo.begin() + itemDeleteIndex);
+
+						cout << "Deleted: " + itemDelete + "\n";
+					}
+				} break;
 			case 5:
 				cout << "Goodbye!\n";
 				// using return instead of exit() since I'm in the main function
@@ -118,11 +141,9 @@ int main () {
 				// good defensive measure even if you don't need the 'break' statement
 				break;
 			default:
-				cout << "*** INVALID INPUT: INTEGER OUT OF RANGE ***\n";
+				cout << "\n*** INVALID INPUT: INDEX OUT OF RANGE ***\n";
 				break;
 		}
-
 	}
 	return 0;
 }
-
